5837689c4f0bc3e963feafdf083f7eef369d6404
Kevin Imvoo <imvooz@gmail.com>
2017-03-18 23:38:14 +1100

    Hide buttons on photo capture.

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -3,10 +3,6 @@ import { StyleSheet, TouchableHighlight, Text, View, Image, StatusBar, Button } 
 import { TabNavigator } from 'react-navigation';
 import { NavigationButton } from '../NavigationButton';
 
-import {takeSnapshot} from "react-native-view-shot";
-
-// import Expo from 'expo';
-
 import Camera from "react-native-camera";
 
 import { styles } from '../Styles';
@@ -27,10 +23,6 @@ export class MainScreen extends React.Component {
   render() {
     console.log(this.state);
 
-    let imageURL = {  
-      uri: "https://tctechcrunch2011.files.wordpress.com/2015/04/codecode.jpg"
-    };
-
     var imageOn = null;
 
     if (this.state.image != null) {
@@ -48,7 +40,7 @@ export class MainScreen extends React.Component {
           style={StyleSheet.absoluteFill}
           aspect={Camera.constants.Aspect.fill}
           captureTarget={Camera.constants.CaptureTarget.disk}
-          captureQuality={"480p"}
+          captureQuality={"720p"}
           orientation={"portrait"}
           >
         </Camera>
@@ -57,18 +49,18 @@ export class MainScreen extends React.Component {
 
         {imageOn}
         
-        <TouchableHighlight onPress={this.takePicture.bind(this)} style={{ zIndex: 2, alignItems: 'center', position: 'absolute', bottom: 50, left: 0, right: 0}}>
+        { this.state.image == null && <TouchableHighlight onPress={this.takePicture.bind(this)} style={{ zIndex: 2, alignItems: 'center', position: 'absolute', bottom: 50, left: 0, right: 0}}>
           <View style={{height: 50, width: 50, borderRadius: 128, backgroundColor: '#232528', opacity:0.7}}>
             <View style={{left: 2, top: 2, height: 46, width: 46, borderRadius: 128, backgroundColor: 'white', opacity: 0.7}}>
               
             </View>
           </View>
-        </TouchableHighlight>
+        </TouchableHighlight> }
 
         <NavigationButton navigation={this.props.navigation} styleType={"SettingsButton"} name={"⚙"} link={"Settings"} />
         <StatusBar hidden={false} />
 
-        <NavigationButton navigation={this.props.navigation} styleType={"ViewQueueButton"} name={"View Queue"} link={"Queue"} />
+        { this.state.image == null && <NavigationButton navigation={this.props.navigation} styleType={"ViewQueueButton"} name={"View Queue"} link={"Queue"} /> }
       </View>
     );
   }
