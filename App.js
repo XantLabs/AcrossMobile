b23de17b55d4f5542b6bef74dbbf98130ec49c8f
garwahl <garwahlam@gmail.com>
2017-03-18 17:15:09 +1100

    Fixed Merge conflicts

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import {
   AppRegistry,
   Dimensions,
@@ -8,26 +7,17 @@ import {
   TouchableHighlight,
   View
 } from 'react-native';
-import PhotoViewer from './src/PhotoViewer'
 
-export default class App extends React.Component {
-  render() {
-    return (
-        <PhotoViewer></PhotoViewer>
-    );
-  }
-}
-=======
 import { TabNavigator } from 'react-navigation';
-
 import { MainScreen } from './src/screens/MainScreen';
 import { SettingsScreen } from './src/screens/SettingsScreen';
 import { QueueScreen } from './src/screens/QueueScreen';
+import PhotoViewer from './src/PhotoViewer';
 
 export default App = TabNavigator({
   Main: {screen: MainScreen},
   Settings: {screen: SettingsScreen},
-  Queue: {screen: QueueScreen}
+  Queue: {screen: PhotoViewer}
 }, {
   navigationOptions: {
     tabBar: {
@@ -35,4 +25,3 @@ export default App = TabNavigator({
     }
   }
 });
->>>>>>> ad356df81e657bc8689c4df246a11a745ef7c815
