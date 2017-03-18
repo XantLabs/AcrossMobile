96b88a189c56c1cd1b2819ecedc2ae466e9e95e1
Kevin Imvoo <imvooz@gmail.com>
2017-03-18 15:15:55 +1100

    Add queue screen.
    Hide tab bar.

--- App.js
+++ App.js
@@ -3,8 +3,16 @@ import { TabNavigator } from 'react-navigation';
 
 import { MainScreen } from './src/screens/MainScreen';
 import { SettingsScreen } from './src/screens/SettingsScreen';
+import { QueueScreen } from './src/screens/QueueScreen';
 
 export default App = TabNavigator({
   Main: {screen: MainScreen},
-  Settings: {screen: SettingsScreen}
+  Settings: {screen: SettingsScreen},
+  Queue: {screen: QueueScreen}
+}, {
+  navigationOptions: {
+    tabBar: {
+      visible: false
+    }
+  }
 });
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, StatusBar } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import { NavigationButton } from '../NavigationButton';
 
